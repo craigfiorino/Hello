d5be57b2f40e5d3cbd552e301d589f02377a1978
craig fiorino <craigfiorino@billericak12.com>
2021-09-10 11:34:21 -0400

    sagjssjafjasjjsjfsjfssh

---
+++ src/com/company/YummyFood.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class YummyFood {
+    public static void main(String[] args) {
+        System.out.print("Yummy ");
+        System.out.println("Food");
+        chocolate();
+        chocolate();
+        iceCream();
+        System.out.println("done");
+    }
+    public static void chocolate(){
+        System.out.println("Chocolate");
+    }
+
+    public static void iceCream() {
+        chocolate();
+        System.out.println("that was yummy");
+    }
+}
