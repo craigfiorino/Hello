d8b2f283fc955a57c0efd01d77caa2b71a08fd6f
Craig Fiorino <craigfiorino@billericak12.com>
2021-09-20 12:05:14 -0400

    gjfdgdghdgd

--- src/com/company/Mathx.java
+++ src/com/company/Mathx.java
@@ -8,9 +8,11 @@ public class Mathx {
     }
     public static int swap(int input){
         System.out.println(input);
-        System.out.println(input-(input % 100) +10* (input % 10)+0.1*(input % 1)+(0.1*((input-(input % 10))-(input-(input % 100)))));
-        int un= input-(input % 100) +10* (input % 10);
-        int do= 10*(input % 10)+(input % 1)/10;
+        int un= input-de;
+        int de= 10*(input % 10)+(input % 1)/10;
+        int tr = input-(input % 100);
+        System.out.println(tr+de);
+
         return input;
     }
 }
